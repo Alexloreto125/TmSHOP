11aec2cf76107b36a55b47bb9ea4f004b3d821a1
Alexloreto125 <alexloreto125@gmail.com>
2024-05-13 18:14:13 +0200

    Creazione categoria

---
+++ src/Components/Categoria.jsx
@@ -0,0 +1,10 @@
+import { Row } from "react-bootstrap";
+
+const Categoria = () => {
+  return (
+    <Row className="me-3">
+      <h2>Categoria</h2>
+    </Row>
+  );
+};
+export default Categoria;
