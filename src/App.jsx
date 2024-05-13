97c5bba7722adfb13df571342d52ff919d9dd937
Alexloreto125 <alexloreto125@gmail.com>
2024-05-13 18:14:38 +0200

    montaggio component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import CustomNavBar from "./Components/CustomNavBar";
+import GrigliaItem from "./Components/GrigliaItem";
+import Categoria from "./Components/Categoria";
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
   return (
     <>
       <CustomNavBar />
+      <Container className="d-flex mt-3 ms-2">
+        <Categoria />
+        <GrigliaItem />
+      </Container>
     </>
   );
 }
